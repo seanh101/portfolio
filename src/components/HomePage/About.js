b990b341ee70fe4434a94c126dd09fbfac916324
seanh101 <seanharrington4189@gmail.com>
2023-08-04 13:31:55 -0400

    projects and skills put into grid

--- src/components/HomePage/About.js
+++ src/components/HomePage/About.js
@@ -1,5 +1,7 @@
+// About.js
+
 import React from 'react';
-import './About.css'
+import './About.css';
 
 function About() {
   return (
@@ -13,23 +15,68 @@ function About() {
           <p>
             With experience in JavaScript, React.js, Python, Django, and more, I have the skills to tackle challenging problems and deliver high-quality code. I thrive on attention to detail, effective communication, and a user-centric mindset to create solutions that meet both technical requirements and user needs.
           </p>
+          <p>
+            During my journey as a software engineer, I've worked on several exciting projects. Let me highlight a few of them:
+          </p>
+          
         </div>
       </div>
 
       <div className="row mt-5">
         <div className="col-lg-8 offset-lg-2">
           <h2>Projects</h2>
-          <ul>
-            <li>
-              <strong>Couch Potato:</strong> Built using the MERN stack, Couch Potato is an application that allows users to search for movies and shows and find out where they are streaming. It features efficient data storage using MongoDB and a seamless user experience.
-            </li>
-            <li>
-              <strong>Soundify:</strong> Soundify is a playlist and song management web app developed with Django and Python. It offers full CRUD functionality for playlists and songs, authentication and authorization using Django's built-in system, and integration with the Spotify API for song information.
-            </li>
-            <li>
-              <strong>Blackjack:</strong> While not mentioned in the resume, I also worked on a Blackjack game project where I implemented the game logic, user interface, and interactive gameplay using JavaScript. It was an exciting project that showcased my problem-solving and programming skills.
-            </li>
-          </ul>
+          <div className="project-grid">
+            <div className="project-item">
+              <h3>Couch Potato</h3>
+              <p>
+                Built using the MERN stack, Couch Potato is an application that allows users to search for movies and shows and find out where they are streaming. It features efficient data storage using MongoDB and a seamless user experience.
+              </p>
+              <div className="skills-grid">
+                <div className="skill-item">MERN Stack</div>
+                <div className="skill-item">Watchmode API</div>
+                <div className="skill-item">Movieofthenight API</div>
+                <div className="skill-item">React</div>
+                <div className="skill-item">JavaScript</div>
+                <div className="skill-item">CSS</div>
+              </div>
+            </div>
+            <div className="project-item">
+              <h3>Soundify</h3>
+              <p>
+                Soundify is a playlist and song management web app developed with Django and Python. It offers full CRUD functionality for playlists and songs, authentication and authorization using Django's built-in system, and integration with the Spotify API for song information.
+              </p>
+              <div className="skills-grid">
+                <div className="skill-item">Django</div>
+                <div className="skill-item">Python</div>
+                <div className="skill-item">JavaScript</div>
+                <div className="skill-item">HTML</div>
+                <div className="skill-item">CSS</div>
+                <div className="skill-item">PostgreSQL</div>
+                <div className="skill-item">Trello</div>
+                <div className="skill-item">Git</div>
+                <div className="skill-item">AWS</div>
+                <div className="skill-item">Spotify API</div>
+              </div>
+            </div>
+            <div className="project-item">
+              <h3>NBA Lineup Creator</h3>
+              <p>
+                The NBA Lineup Creator is a web app built with Node.js, Express, and MongoDB. It allows users to create and manage their own NBA lineups using real player data from the balldontlie API.
+              </p>
+              <div className="skills-grid">
+                <div className="skill-item">Node.js</div>
+                <div className="skill-item">Express</div>
+                <div className="skill-item">Mongoose</div>
+                <div className="skill-item">MongoDB</div>
+                <div className="skill-item">JavaScript</div>
+                <div className="skill-item">HTML/CSS</div>
+                <div className="skill-item">Balldontlie API</div>
+                <div className="skill-item">Git</div>
+                <div className="skill-item">Trello</div>
+                <div className="skill-item">0auth</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -46,5 +93,6 @@ function About() {
 }
 
 export default About;
+
 
 
